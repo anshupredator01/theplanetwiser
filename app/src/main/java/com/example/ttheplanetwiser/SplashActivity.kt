670dbb387bdf488ca8916a08707d6712da0456d0
Anshu Kumar Pandey <anshupredator01@gmail.com>
2022-12-23 18:26:58 +0530

    Success

---
+++ app/src/main/java/com/example/ttheplanetwiser/SplashActivity.kt
@@ -0,0 +1,27 @@
+package com.example.ttheplanetwiser
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Intent
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowManager
+
+
+@Suppress("DEPRECATION")
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
